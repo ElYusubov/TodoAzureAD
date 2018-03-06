121bdafe8705a0df00d730b057773399a8fa0e98
EY <elxany@hotmail.com>
2018-03-05 23:02:22 -0500

    Removed login enforcement fro Home page. Minor changes added to the layout.

--- MyAzureAD/Controllers/HomeController.cs
+++ MyAzureAD/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ namespace TaskTracker.Controllers
 {   
     public class HomeController : Controller
     {
-        [Authorize]
+        
         public ActionResult Index()
         {
             return View();
